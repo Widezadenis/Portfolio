4438ec80f7a1070be538245b55ba2fe4ee02655a
Wideza Denis <Widezad@gmail.com>
2022-05-22 16:36:59 -0400

    Add files via upload

---
+++ LeuxCheveuxLuxe/js/main.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('.hamburger');
+const mainMenu = document.querySelector('.mainMenu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle("active");
+    mainMenu.classList.toggle("active");
+})
+
+document.querySelectorAll("nav-link").forEach(n => n 
+    .addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        mainMenu.classList.remove("active");
+    }))
